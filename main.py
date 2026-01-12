6f83367c7d14c78277756b865e5d2ba458507d37
Chrahuldeveloper <chrahulofficial@gmail.com>
2026-01-12 20:26:53 +0530

    retrival working

--- main.py
+++ main.py
@@ -25,13 +25,17 @@ chunks = text_splitter.split_text(full_text)
 
 embeddings = model.encode(chunks, convert_to_numpy=True)
 
-print(f"Chunks: {len(chunks)}")
-print(f"Embedding shape: {embeddings.shape}")
-
 collection.add(
     documents=chunks,
     embeddings=embeddings.tolist(),
     ids=[str(i) for i in range(len(chunks))]
 )
 
-print("✅ Stored in ChromaDB")
+question = "what are the plans available"
+question_embedding = model.encode(question, convert_to_numpy=True)
+
+res = collection.query(
+    query_embeddings=[question_embedding],
+    n_results=3
+)
+print(res["documents"])
